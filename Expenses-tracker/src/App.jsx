b238db32e67c0a63a5967542e083b7d1e2d5a752
dabek782 <kubadabrowski782@gmail.com>
2025-10-31 14:34:52 +0100

    initial commit

---
+++ Expenses-tracker/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+function App() {
+  return (
+    <div className="flex min-h-svh min-w-screen flex-col bg-amber-800 items-center justify-center">
+      <Button >Click me</Button>
+    </div>
+  )
+}
+
+export default App
